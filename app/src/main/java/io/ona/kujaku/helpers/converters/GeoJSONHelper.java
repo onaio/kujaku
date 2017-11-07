e0fe585a6009d75aef1746bd07da955d2032927b
Jason Rogena <jasonrogena@gmail.com>
2017-11-07 10:57:47 +0300

    EK|JR: GeoJSON Helper Class Code Cleanup
    Clean code in GeoJSONFeature and GeoJSONHelper classes.
    
    Fixes issue #2
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

--- app/src/main/java/io/ona/kujaku/helpers/converters/GeoJSONHelper.java
+++ app/src/main/java/io/ona/kujaku/helpers/converters/GeoJSONHelper.java
@@ -10,15 +10,34 @@ import com.cocoahero.android.geojson.Point;
 import com.cocoahero.android.geojson.Position;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by Ephraim Kigamba on 06/11/2017.
+ * GeoJSON factory that converts {@link GeoJSONFeature} into a MapBox's GeoJSON data-source or a
+ * GeoJSON feature-collection.
+ *
+ * See:
+ *  https://www.mapbox.com/mapbox-gl-js/style-spec/#sources-geojson
+ *  https://geojson.org/geojson-spec.html
+ *
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 06/11/2017.
  */
 
 public class GeoJSONHelper {
-
+    private static final String TAG = GeoJSONHelper.class.getSimpleName();
+    public static final String MAPBOX_GEOJSON_DATASOURCE_TYPE = "geojson";
+    public static final JSONObject DEFAULT_FEATURE_COLLECTION;
+    static {
+        DEFAULT_FEATURE_COLLECTION = new JSONObject();
+        try {
+            DEFAULT_FEATURE_COLLECTION.put("type", GeoJSON.TYPE_FEATURE_COLLECTION);
+            DEFAULT_FEATURE_COLLECTION.put("features", new JSONArray());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
     private FeatureCollection featureCollection;
 
     /**
@@ -35,12 +54,10 @@ public class GeoJSONHelper {
         }
 
         for(GeoJSONFeature geoJSONFeature : geoJSONFeatures) {
-
             try {
                 featureCollection.addFeature(getFeature(geoJSONFeature));
             } catch (JSONException e) {
-                e.printStackTrace();
-                Log.e(GeoJSONHelper.class.getName(), Log.getStackTraceString(e));
+                Log.e(TAG, Log.getStackTraceString(e));
             }
         }
     }
@@ -49,36 +66,37 @@ public class GeoJSONHelper {
         this.featureCollection = featureCollection;
     }
 
-    public String getJsonFeatureCollection() {
-        if (featureCollection == null) {
-            return "";
+    private JSONObject getFeatureCollectionJson() throws JSONException {
+        if (featureCollection != null) {
+            return featureCollection.toJSON();
         }
 
-        try {
-            return featureCollection.toJSON().toString();
-        } catch (JSONException e) {
-            e.printStackTrace();
-            Log.e(GeoJSONHelper.class.getName(), Log.getStackTraceString(e));
-            return "";
-        }
+        return DEFAULT_FEATURE_COLLECTION;
     }
 
-    public String getGeoJsonData() {
-        if (featureCollection == null) {
-            return "";
-        }
+    /**
+     * Returns a string representation of the feature collection
+     *
+     * @return  String representation of the feature collection or an empty feature collection if
+     *          {@link GeoJSONHelper#featureCollection} is {@code NULL}
+     * @throws JSONException If unable to create the feature collection JSONObject
+     */
+    public String getJsonFeatureCollection() throws JSONException {
+        return getFeatureCollectionJson().toString();
+    }
 
+    /**
+     * Returns the feature collection wrapped in a MapBox's GeoJSON data source object.
+     *
+     * @return String representation of the feature collection wrapped in MapBox's GeoJSON data
+     *         source or an empty string if {@link GeoJSONHelper#featureCollection} is {@code null}
+     */
+    public String getGeoJsonData() throws JSONException {
         JSONObject geoJson = new JSONObject();
+        geoJson.put("type", MAPBOX_GEOJSON_DATASOURCE_TYPE);
+        geoJson.put("data", getFeatureCollectionJson());
 
-        try {
-            geoJson.put("type", "geojson");
-            geoJson.put("data", featureCollection.toJSON());
-            return geoJson.toString();
-        } catch (JSONException e) {
-            e.printStackTrace();
-            Log.e(GeoJSONHelper.class.getName(), Log.getStackTraceString(e));
-            return "";
-        }
+        return geoJson.toString();
     }
 
     private Feature getFeature(GeoJSONFeature geoJSONFeature) throws JSONException {
