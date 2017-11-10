c0404628c12db3e6941740b887dcea24f6d47888
Ephraim Kigamba <ekigamba@ona.io>
2017-11-10 12:17:07 +0300

    Add MapBoxStyleStorage tests

---
+++ library/src/test/java/io/ona/kujaku/helpers/MapBoxStyleStorageTest.java
@@ -0,0 +1,97 @@
+package io.ona.kujaku.helpers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+import io.ona.kujaku.BuildConfig;
+
+/**
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 10/11/2017.
+ */
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class,manifest=Config.NONE)
+public class MapBoxStyleStorageTest {
+
+    @Test
+    public void getStyleUrlShouldReturnAsset() {
+        String assetUrl = "asset://sample_asset.json";
+        MapBoxStyleStorage mapBoxStyleStorage = new MapBoxStyleStorage();
+        String resultUrl = mapBoxStyleStorage.getStyleURL(assetUrl);
+
+        assertEquals(assetUrl, resultUrl);
+    }
+
+    @Test
+    public void getStyleUrlShouldReturnUrl() {
+        String unsecureNetworkUrl = "http://mycompany.com/style.json";
+        MapBoxStyleStorage mapBoxStyleStorage = new MapBoxStyleStorage();
+        String resultUrl = mapBoxStyleStorage.getStyleURL(unsecureNetworkUrl);
+
+        assertEquals(unsecureNetworkUrl, resultUrl);
+
+        String secureNetworkUrl = "https://mycompany.com/style.json";
+        resultUrl = mapBoxStyleStorage.getStyleURL(secureNetworkUrl);
+
+        assertEquals(secureNetworkUrl, resultUrl);
+    }
+
+    @Test
+    public void getStyleUrlShouldReturnLocalFile() {
+        String localFileUrl = "file:///sdcard/SampleFolder/style.json";
+        MapBoxStyleStorage mapBoxStyleStorage = new MapBoxStyleStorage();
+        String resultUrl = mapBoxStyleStorage.getStyleURL(localFileUrl);
+
+        assertEquals(localFileUrl, resultUrl);
+    }
+
+    @Test
+    public void getStyleUrlShouldReturnNewLocalFile() {
+        String mapboxStyle = "{ 'version': 8, 'name': 'kujaku-map', 'metadata': {}, }";
+        MapBoxStyleStorage mapBoxStyleStorage = new MapBoxStyleStorage();
+        String resultUrl = mapBoxStyleStorage.getStyleURL(mapboxStyle);
+
+        File createdFile = new File(resultUrl.replace("file://", ""));
+
+        assertTrue(resultUrl.startsWith("file://"));
+        assertTrue(createdFile.exists());
+    }
+
+    @Test
+    public void deleteFileShouldReturnTrue() {
+        String mapboxStyle = "{ 'version': 8, 'name': 'kujaku-map', 'metadata': {}, }";
+        MapBoxStyleStorage mapBoxStyleStorage = new MapBoxStyleStorage();
+        String resultUrl = mapBoxStyleStorage.getStyleURL(mapboxStyle);
+
+        File file = new File(resultUrl.replace("file://", ""));
+
+        assertTrue(file.exists());
+
+        boolean isDeleted = mapBoxStyleStorage.deleteFile(resultUrl.replace("file://", ""));
+        assertTrue(isDeleted);
+        assertFalse(file.exists());
+    }
+
+    @Test
+    public void deleteFileShouldReturnFail() {
+        String mapboxStyle = "{ 'version': 8, 'name': 'kujaku-map', 'metadata': {}, }";
+        MapBoxStyleStorage mapBoxStyleStorage = new MapBoxStyleStorage();
+        String resultUrl = mapBoxStyleStorage.getStyleURL(mapboxStyle);
+
+        File file = new File(resultUrl.replace("file://", ""));
+
+        assertTrue(file.exists());
+
+        boolean isDeleted = mapBoxStyleStorage.deleteFile(resultUrl);
+        assertFalse(isDeleted);
+        assertTrue(file.exists());
+    }
+    
+    
+}
