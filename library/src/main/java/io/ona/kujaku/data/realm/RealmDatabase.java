799ee240b607b84011430cd6864fbbbf39a51852
Ephraim Kigamba <ekigamba@ona.io>
2018-01-19 15:52:44 +0300

    Refactor code, add documentation & add tests

--- library/src/main/java/io/ona/kujaku/data/realm/RealmDatabase.java
+++ library/src/main/java/io/ona/kujaku/data/realm/RealmDatabase.java
@@ -3,6 +3,8 @@ package io.ona.kujaku.data.realm;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
+import com.mapbox.mapboxsdk.offline.OfflineRegion;
+
 import io.ona.kujaku.data.realm.objects.MapBoxOfflineQueueTask;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -11,7 +13,6 @@ import io.realm.RealmResults;
 /**
  * Created by Jason Rogena - jrogena@ona.io on 11/20/17.
  */
-
 public class RealmDatabase {
     protected static final long VERSION = 1l;
     protected static final String NAME = "kujaku.realm";
@@ -59,6 +60,42 @@ public class RealmDatabase {
         }
     }
 
+    /**
+     * Saves a {@link MapBoxOfflineQueueTask} as {@link MapBoxOfflineQueueTask#TASK_STATUS_DONE}<br/>
+     * This means the {@link OfflineRegion} can no longer be resumed if it was incomplete.<br/>
+     * This also means that a {@link OfflineRegion} will still be in storage if it was not successfully deleted
+     *
+     * @param mapBoxOfflineQueueTask
+     */
+    public void persistCompletedStatus(@NonNull MapBoxOfflineQueueTask mapBoxOfflineQueueTask) {
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+
+        mapBoxOfflineQueueTask.setTaskStatus(MapBoxOfflineQueueTask.TASK_STATUS_DONE);
+        realm.commitTransaction();
+    }
+
+    /**
+     * Updates the {@link MapBoxOfflineQueueTask} status as {@link MapBoxOfflineQueueTask#TASK_STATUS_NOT_STARTED}<br/>
+     * This means that subsequent Offline Region download calls with a similar map name will not result in this task being deleted.
+     *
+     * @param mapBoxOfflineQueueTask
+     */
+    public void persistDownloadStartedStatus(@NonNull MapBoxOfflineQueueTask mapBoxOfflineQueueTask) {
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        mapBoxOfflineQueueTask.setTaskStatus(MapBoxOfflineQueueTask.TASK_STATUS_STARTED);
+
+        realm.commitTransaction();
+    }
+
+    /**
+     * Retrieves the {@link MapBoxOfflineQueueTask}s of type {@link MapBoxOfflineQueueTask#TASK_TYPE_DOWNLOAD}
+     * which are pending i.e. their downloads have not started. Their status is {@link MapBoxOfflineQueueTask#TASK_STATUS_NOT_STARTED}
+     *
+     * @param mapName
+     * @return
+     */
     protected RealmResults<MapBoxOfflineQueueTask> getPendingOfflineMapDownloadsWithSimilarNames(String mapName) {
         Realm realm = Realm.getDefaultInstance();
 
@@ -70,6 +107,13 @@ public class RealmDatabase {
         return mapBoxOfflineQueueTaskRealmResults;
     }
 
+    /**
+     * Deletes all {@link MapBoxOfflineQueueTask}s of type {@link MapBoxOfflineQueueTask#TASK_TYPE_DOWNLOAD}
+     * whose download has not started
+     *
+     * @param mapName
+     * @return
+     */
     public boolean deletePendingOfflineMapDownloadsWithSimilarNames(String mapName) {
         RealmResults<MapBoxOfflineQueueTask> realmResults = getPendingOfflineMapDownloadsWithSimilarNames(mapName);
 
