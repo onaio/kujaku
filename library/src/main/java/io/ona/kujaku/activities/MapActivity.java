7939a60b899485967de26c3bbcd8c6dd0d1ed162
Ephraim Kigamba <ekigamba@ona.io>
2018-01-24 10:10:18 +0300

    Code cleanup

--- library/src/main/java/io/ona/kujaku/activities/MapActivity.java
+++ library/src/main/java/io/ona/kujaku/activities/MapActivity.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Build;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
