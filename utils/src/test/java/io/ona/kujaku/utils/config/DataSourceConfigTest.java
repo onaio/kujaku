e68b2923083d4bd0c7502d0d4cfef4d4616952f4
Jason Rogena <jasonrogena@gmail.com>
2018-01-30 14:47:28 +0300

    Interchange equalsTo Assertions in DataSourceConfigTest
    Interchange equalsTo assertions in DataSourceConfigTest to
    assertEquals(expected, actual) to avoid confusion.
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

--- utils/src/test/java/io/ona/kujaku/utils/config/DataSourceConfigTest.java
+++ utils/src/test/java/io/ona/kujaku/utils/config/DataSourceConfigTest.java
@@ -57,12 +57,12 @@ public class DataSourceConfigTest {
 
         String name2 = "test";
         DataSourceConfig config1 = new DataSourceConfig(name2);
-        Assert.assertEquals(config1.getName(), name2);
+        Assert.assertEquals(name2, config1.getName());
 
         JSONObject object2 = new JSONObject();
         object2.put(DataSourceConfig.KEY_NAME, name2);
         DataSourceConfig  config2 = new DataSourceConfig(object2);
-        Assert.assertEquals(config2.getName(), name2);
+        Assert.assertEquals(name2, config2.getName());
     }
 
     @Test
@@ -86,7 +86,7 @@ public class DataSourceConfigTest {
         object1.put(DataSourceConfig.KEY_NAME, name1);
         DataSourceConfig config1 = new DataSourceConfig(object1);
         // Assert that the resultant JSONObject is similar to what was passed in the constructor
-        Assert.assertEquals(config1.toJsonObject().toString(), object1.toString());
+        Assert.assertEquals(object1.toString(), config1.toJsonObject().toString());
         // Make sure the churned out JSONObject doesn't refer the same object passed in the constructor
         Assert.assertNotSame(object1, config1.toJsonObject());
     }
@@ -94,7 +94,7 @@ public class DataSourceConfigTest {
     @Test
     public void testExtractDataSourceNames() throws InvalidMapBoxStyleException {
         // Assert that the default size is 0
-        Assert.assertEquals(DataSourceConfig.extractDataSourceNames(new ArrayList<DataSourceConfig>()).length, 0);
+        Assert.assertEquals(0, DataSourceConfig.extractDataSourceNames(new ArrayList<DataSourceConfig>()).length);
 
         ArrayList<DataSourceConfig> configs1 = new ArrayList<>();
         int size1 = 6;
@@ -103,10 +103,10 @@ public class DataSourceConfigTest {
         }
         // Assert the size of the generated list
         String[] names1 = DataSourceConfig.extractDataSourceNames(configs1);
-        Assert.assertEquals(names1.length, size1);
+        Assert.assertEquals(size1, names1.length);
         // Assert the order of names
         for (int i = 0; i < names1.length; i++) {
-            Assert.assertEquals(names1[i], String.valueOf(i));
+            Assert.assertEquals(String.valueOf(i), names1[i]);
         }
     }
 }
