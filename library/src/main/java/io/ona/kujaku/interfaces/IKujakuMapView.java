8ffa5bb162b156e39592395cbcff1219a3ae2068
Ephraim Kigamba <ekigamba@ona.io>
2018-09-26 17:57:39 +0300

    Add interface documentation

--- library/src/main/java/io/ona/kujaku/interfaces/IKujakuMapView.java
+++ library/src/main/java/io/ona/kujaku/interfaces/IKujakuMapView.java
@@ -10,6 +10,15 @@ import io.ona.kujaku.callbacks.AddPointCallback;
 
 public interface IKujakuMapView extends IKujakuMapViewLowLevel {
 
+    /**
+     * Enables adding a point on the map. You can either use GPS by passing a {@code true} on @param useGPS
+     * which disables the scrolling and animates the location updates on the map while showing the location accuracy
+     * until the user selects a certain position.
+     *
+     * @param useGPS
+     * @param addPointCallback returns the location chosen by user from GPS or calls {@link AddPointCallback#onCancel()}
+     *                         if the user cancels the operation
+     */
     void addPoint(boolean useGPS, @NonNull AddPointCallback addPointCallback);
 
 }
--- library/src/main/java/io/ona/kujaku/interfaces/IKujakuMapViewLowLevel.java
+++ library/src/main/java/io/ona/kujaku/interfaces/IKujakuMapViewLowLevel.java
@@ -1,5 +1,8 @@
 package io.ona.kujaku.interfaces;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 import com.google.android.gms.maps.model.LatLng;
 
 import org.json.JSONObject;
@@ -12,9 +15,39 @@ import io.ona.kujaku.listeners.OnLocationChanged;
 
 public interface IKujakuMapViewLowLevel {
 
+    /**
+     * Enables/disables the marker layout so that the user can scroll to their preferred location
+     *
+     * @param canAddPoint
+     */
     void enableAddPoint(boolean canAddPoint);
 
-    void enableAddPoint(boolean canAddPoint, OnLocationChanged onLocationChanged);
+    /**
+     * Call this to enable/disable adding a new point using GPS. In this layout the user cannot
+     * scroll since the marker position is guided by the GPS updates. Disabling it disables the marker
+     * layout and GPS querying.
+     *
+     * @param canAddPoint
+     * @param onLocationChanged
+     */
+    void enableAddPoint(boolean canAddPoint, @NonNull OnLocationChanged onLocationChanged);
 
-    JSONObject dropPoint(LatLng latLng);
+    /**
+     * This should be called after calling {@link #enableAddPoint(boolean)} with {@code true} thus
+     * disabling the marker layout, adding a point at the current marker position & returns a geoJSON
+     * feature of the current marker position.
+     *
+     * @return
+     */
+    JSONObject dropPoint();
+
+    /**
+     * This should be called after calling {@link #enableAddPoint(boolean, OnLocationChanged)} with {@code true} thus
+     * disabling the marker layout, disabling GPS location updates, adding a point at @param latLng & returns a geoJSON
+     * feature at @param latLng
+     *
+     * @param latLng
+     * @return
+     */
+    JSONObject dropPoint(@Nullable LatLng latLng);
 }
