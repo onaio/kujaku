3704f377222b3e5b551c7f016501dd44ae2fe5c8
Ephraim Kigamba <ekigamba@ona.io>
2018-11-05 15:29:28 +0300

    Add KujakuMapView tests for ADD POINT API
    Fixes #165

---
+++ library/src/test/java/io/ona/kujaku/test/shadows/ShadowGeoJsonSource.java
@@ -0,0 +1,19 @@
+package io.ona.kujaku.test.shadows;
+
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+
+/**
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 05/11/2018
+ */
+
+@Implements(GeoJsonSource.class)
+public class ShadowGeoJsonSource {
+
+    @Implementation
+    protected void initialize(String layerId, Object options) {
+
+    }
+}
