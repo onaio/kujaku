569d5da735d4512f864e07b25a150d3fbdcfbcfb
Vincent Karuri <vincentmkaruri@gmail.com>
2018-11-12 12:15:16 +0300

    Create separate demo activity for card views

--- sample/src/main/java/io/ona/kujaku/sample/activities/CardActivity.java
+++ sample/src/main/java/io/ona/kujaku/sample/activities/CardActivity.java
@@ -1,31 +1,94 @@
 package io.ona.kujaku.sample.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 
+import com.mapbox.mapboxsdk.Mapbox;
+
+import org.json.JSONObject;
+
+import io.ona.kujaku.callbacks.AddPointCallback;
+import io.ona.kujaku.sample.BuildConfig;
 import io.ona.kujaku.sample.R;
+import io.ona.kujaku.views.KujakuMapView;
 
-public class CardActivity extends AppCompatActivity {
+public class CardActivity extends BaseNavigationDrawerActivity {
+
+    private static final String TAG = LowLevelLocationAddPointMapView.class.getName();
+    private KujakuMapView kujakuMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_card);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Mapbox.getInstance(this, BuildConfig.MAPBOX_SDK_ACCESS_TOKEN);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        kujakuMapView = findViewById(R.id.card_activity_map_view);
+
+        kujakuMapView.showCurrentLocationBtn(true);
+
+        kujakuMapView.addPoint(false, new AddPointCallback() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onPointAdd(JSONObject jsonObject) {
+                Log.i(TAG, jsonObject.toString());
+                // We should probably save the points here
+            }
+
+            @Override
+            public void onCancel() {
+                // Oops, the user cancelled the operation, we can just finish the activity (and save the points here)
             }
         });
     }
 
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_card;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (kujakuMapView != null) kujakuMapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (kujakuMapView != null) kujakuMapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (kujakuMapView != null) kujakuMapView.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (kujakuMapView != null) kujakuMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (kujakuMapView != null) kujakuMapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (kujakuMapView != null) kujakuMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        if (kujakuMapView != null) kujakuMapView.onLowMemory();
+    }
+
+    @Override
+    protected int getSelectedNavigationItem() {
+        return R.id.nav_card_activity;
+    }
 }
