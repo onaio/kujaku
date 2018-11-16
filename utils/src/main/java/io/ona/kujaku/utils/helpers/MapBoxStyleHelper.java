00ef3a8255137bf1520bd089206096cd6171c30b
Ephraim Kigamba <ekigamba@ona.io>
2018-11-16 17:50:30 +0300

    Remove unused import

--- utils/src/main/java/io/ona/kujaku/utils/helpers/MapBoxStyleHelper.java
+++ utils/src/main/java/io/ona/kujaku/utils/helpers/MapBoxStyleHelper.java
@@ -2,7 +2,6 @@ package io.ona.kujaku.utils.helpers;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 import com.mapbox.core.utils.TextUtils;
 import com.mapbox.mapboxsdk.geometry.LatLng;
