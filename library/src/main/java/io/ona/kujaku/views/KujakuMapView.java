be525217c4b8ac774d605946d5f5866982ccfd94
Ephraim Kigamba <ekigamba@ona.io>
2018-11-27 15:55:36 +0300

    Add tests for bounds listener

--- library/src/main/java/io/ona/kujaku/views/KujakuMapView.java
+++ library/src/main/java/io/ona/kujaku/views/KujakuMapView.java
@@ -9,6 +9,7 @@ import android.os.AsyncTask;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -552,8 +553,9 @@ public class KujakuMapView extends MapView implements IKujakuMapView {
         }
     }
 
+    @VisibleForTesting
     @Nullable
-    private VisibleRegion getCurrentBounds() {
+    protected VisibleRegion getCurrentBounds() {
         return mapboxMap != null ? mapboxMap.getProjection().getVisibleRegion() : null;
     }
 
