48338464912885e33b78f5f869ac01c5d2083fc9
Vincent Karuri <vincentmkaruri@gmail.com>
2018-12-04 15:30:59 +0300

    1. Add logic to add points on map based on bounds 2. Minor layout refactor

--- sample/src/main/java/io/ona/kujaku/sample/activities/BoundsAwareActivity.java
+++ sample/src/main/java/io/ona/kujaku/sample/activities/BoundsAwareActivity.java
@@ -1,17 +1,30 @@
 package io.ona.kujaku.sample.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.style.expressions.Expression;
+import com.mapbox.mapboxsdk.style.layers.Layer;
 
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.ona.kujaku.listeners.BoundsChangeListener;
 import io.ona.kujaku.sample.BuildConfig;
 import io.ona.kujaku.sample.R;
 import io.ona.kujaku.views.KujakuMapView;
+
+import static io.ona.kujaku.sample.utils.TestDataUtils.createFeatureList;
+import static io.ona.kujaku.sample.utils.TestDataUtils.generateMapBoxLayer;
 
 public class BoundsAwareActivity extends BaseNavigationDrawerActivity {
 
@@ -19,11 +32,65 @@ public class BoundsAwareActivity extends BaseNavigationDrawerActivity {
 
     private KujakuMapView kujakuMapView;
 
+    private boolean isBoundsAware = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, BuildConfig.MAPBOX_SDK_ACCESS_TOKEN);
+
         kujakuMapView = findViewById(R.id.bounds_aware_activity_map_view);
+        kujakuMapView.setBoundsChangeListener(new BoundsChangeListener() {
+            @Override
+            public void onBoundsChanged(LatLng topLeft, LatLng topRight, LatLng bottomRight, LatLng bottomLeft) {
+                if (isBoundsAware) {
+                    Log.i(TAG, "Recording bounds change!");
+                    addPointsToMap(calculateCenterCoordinates(topLeft, bottomRight));
+                }
+            }
+        });
+
+        Button btnToggleBoundsListener = findViewById(R.id.btn_toggle_bounds_listener);
+        btnToggleBoundsListener.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isBoundsAware = !isBoundsAware;
+            }
+        });
+
+        initializeGenericLayer();
+    }
+
+
+    private LatLng calculateCenterCoordinates(LatLng topLeft, LatLng bottomRight) {
+        double longitude = (topLeft.getLongitude() + bottomRight.getLongitude()) / 2.0;
+        double latitude = (topLeft.getLatitude() + bottomRight.getLatitude()) / 2.0;
+        return new LatLng(latitude, longitude);
+    }
+
+    private void initializeGenericLayer() {
+        kujakuMapView.initializePrimaryGeoJsonSource("kujaku_primary_source", false, null);
+        Layer circleLayer = generateMapBoxLayer("kujaku_primary_layer", kujakuMapView.getPrimaryGeoJsonSource().getId());
+        kujakuMapView.setPrimaryLayer(circleLayer);
+        // set camera position
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(-1.284956, 36.768831))
+                .zoom(16)
+                .build();
+        kujakuMapView.setCameraPosition(cameraPosition);
+    }
+
+    private void addPointsToMap(LatLng latLng) {
+        try {
+            final String[] featureGroup = {"White", "Black", "Hispanic", "Asian", "Other"};
+            List<Feature> existingFeatures = new ArrayList<>(kujakuMapView.getPrimaryGeoJsonSource().querySourceFeatures(Expression.all()));
+            List<Feature> newFeatures = createFeatureList(20, existingFeatures.size(), latLng.getLongitude(), latLng.getLatitude(), "ethnicity", "Point", false, featureGroup, 0.0009);
+            existingFeatures.addAll(newFeatures);
+            // add features to map
+            kujakuMapView.addFeaturePoints(FeatureCollection.fromFeatures(existingFeatures));
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage());
+        }
     }
 
 
