5198216c29d1ed4d3f4aad9f90c0c42b82e878f8
eotin <eo@novel-t.ch>
2019-01-21 14:51:15 +0100

    We can accept both tags "Style" and "style" in the tiles template URL

--- library/src/main/java/io/ona/kujaku/wmts/model/WmtsResourceUrl.java
+++ library/src/main/java/io/ona/kujaku/wmts/model/WmtsResourceUrl.java
@@ -2,6 +2,8 @@ package io.ona.kujaku.wmts.model;
 
 import org.simpleframework.xml.Attribute;
 
+import java.util.Locale;
+
 /**
  * Describes a Wmts Resource Url object from the WMTS Capabilities object
  *
@@ -30,7 +32,7 @@ public class WmtsResourceUrl {
 
     public static final String TILE_COL_X = "{x}";
     public static final String TILE_ROW_Y = "{y}";
-    public static final String TULE_MATRIX_Z = "{z}";
+    public static final String TILE_MATRIX_Z = "{z}";
 
     @Attribute(name="format")
     private String format;
@@ -40,7 +42,6 @@ public class WmtsResourceUrl {
 
     @Attribute(name="template")
     private String template;
-
 
     public String getFormat() { return this.format; }
 
@@ -52,9 +53,9 @@ public class WmtsResourceUrl {
      */
     public String getTemplate(String styleIdentifier, String tileMatrixSetLinkIdentifier) {
         String _template = this.template.replace(STYLE, styleIdentifier);
+        _template = _template.replace(STYLE.toLowerCase(Locale.ROOT), styleIdentifier); // We accept the style variable "{style}"
         _template = _template.replace(TILE_MATRIX_SET, tileMatrixSetLinkIdentifier);
-
-        _template = _template.replace(TILE_MATRIX, TULE_MATRIX_Z);
+        _template = _template.replace(TILE_MATRIX, TILE_MATRIX_Z);
         _template = _template.replace(TILE_COL, TILE_COL_X);
         _template = _template.replace(TILE_ROW, TILE_ROW_Y);
         return _template;
