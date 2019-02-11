b3c109e617ff30b41551f85b0e0af25949efab07
Ephraim Kigamba <ekigamba@ona.io>
2019-02-11 14:32:02 +0300

    Add ArrowLineLayer tests

---
+++ library/src/test/java/io/ona/kujaku/test/shadows/ShadowLayer.java
@@ -0,0 +1,32 @@
+package io.ona.kujaku.test.shadows;
+
+import android.support.annotation.NonNull;
+
+import com.mapbox.mapboxsdk.style.layers.Layer;
+import com.mapbox.mapboxsdk.style.layers.PropertyValue;
+
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+
+import java.util.HashMap;
+
+/**
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 11/02/2019
+ */
+
+@Implements(Layer.class)
+public class ShadowLayer {
+
+    private HashMap<String, PropertyValue> propertyValues = new HashMap<>();
+
+    @Implementation
+    public void setProperties(@NonNull PropertyValue<?>... properties) {
+        for (PropertyValue propertyValue: properties) {
+            propertyValues.put(propertyValue.name, propertyValue);
+        }
+    }
+
+    public HashMap<String, PropertyValue> getPropertyValues() {
+        return propertyValues;
+    }
+}
