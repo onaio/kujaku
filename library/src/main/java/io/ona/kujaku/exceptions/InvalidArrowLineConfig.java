3955290552a3348d209759d9e645ffb1653a390a
Ephraim Kigamba <ekigamba@ona.io>
2019-02-11 18:10:47 +0300

    Add feature sorting for case relationship visualization

---
+++ library/src/main/java/io/ona/kujaku/exceptions/InvalidArrowLineConfig.java
@@ -0,0 +1,12 @@
+package io.ona.kujaku.exceptions;
+
+/**
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 11/02/2019
+ */
+
+public class InvalidArrowLineConfig extends Exception {
+
+    public InvalidArrowLineConfig(String message) {
+        super(message);
+    }
+}
