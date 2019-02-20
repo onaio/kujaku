67fe999278d772a31316ce7695f876f5770fa4d0
Ephraim Kigamba <ekigamba@ona.io>
2019-02-20 18:51:00 +0300

    Improve last location and location updates accuracy
    * Improve
    wtmp begins Wed Jan 31 09:14  and  accuracy and suitability by using the algorithm provided by google https://developer.android.com/guide/topics/location/strategies\#BestEstimate

--- library/src/main/java/io/ona/kujaku/location/clients/AndroidLocationClient.java
+++ library/src/main/java/io/ona/kujaku/location/clients/AndroidLocationClient.java
@@ -32,6 +32,7 @@ public class AndroidLocationClient extends BaseLocationClient implements Locatio
     private long fastestUpdateInterval = 1000;
 
     private static final String TAG = AndroidLocationClient.class.getName();
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
 
     public AndroidLocationClient(@NonNull Context context) {
         this.context = context;
@@ -134,7 +135,7 @@ public class AndroidLocationClient extends BaseLocationClient implements Locatio
 
             Location latestLocation = null;
             for (Location location : locationResult.getLocations()) {
-                if (latestLocation == null || (location != null && location.getTime() > latestLocation.getTime())) {
+                if (latestLocation == null || (location != null && isBetterLocation(location, latestLocation))) {
                     latestLocation = location;
                 }
             }
@@ -145,6 +146,61 @@ public class AndroidLocationClient extends BaseLocationClient implements Locatio
                     getLocationListener().onLocationChanged(lastLocation);
                 }
             }
+        }
+
+        /** Determines whether one Location reading is better than the current Location fix
+         * @param location  The new Location that you want to evaluate
+         * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+         */
+        private boolean isBetterLocation(Location location, Location currentBestLocation) {
+            if (currentBestLocation == null) {
+                // A new location is always better than no location
+                return true;
+            }
+
+            // Check whether the new location fix is newer or older
+            long timeDelta = location.getTime() - currentBestLocation.getTime();
+            boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+            boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+            boolean isNewer = timeDelta > 0;
+
+            // If it's been more than two minutes since the current location, use the new location
+            // because the user has likely moved
+            if (isSignificantlyNewer) {
+                return true;
+                // If the new location is more than two minutes older, it must be worse
+            } else if (isSignificantlyOlder) {
+                return false;
+            }
+
+            // Check whether the new location fix is more or less accurate
+            int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+            boolean isLessAccurate = accuracyDelta > 0;
+            boolean isMoreAccurate = accuracyDelta < 0;
+            boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+            // Check if the old and new location are from the same provider
+            boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                    currentBestLocation.getProvider());
+
+            // Determine location quality using a combination of timeliness and accuracy
+            if (isMoreAccurate) {
+                return true;
+            } else if (isNewer && !isLessAccurate) {
+                return true;
+            } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+                return true;
+            }
+
+            return false;
+        }
+
+        /** Checks whether two providers are the same */
+        private boolean isSameProvider(String provider1, String provider2) {
+            if (provider1 == null) {
+                return provider2 == null;
+            }
+            return provider1.equals(provider2);
         }
     }
 
