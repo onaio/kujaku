5502a42abea35030b70f14a73c4376c25400202c
Ephraim Kigamba <ekigamba@ona.io>
2019-04-05 16:36:05 +0300

    Enable changing focus area manually foci-boundary & case-relationship activities
    This adds a button to manually change the boundaries and features in the case relationship activity

--- sample/src/main/java/io/ona/kujaku/sample/activities/FociBoundaryActivity.java
+++ sample/src/main/java/io/ona/kujaku/sample/activities/FociBoundaryActivity.java
@@ -2,7 +2,9 @@ package io.ona.kujaku.sample.activities;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.util.Log;
+import android.view.View;
 
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
@@ -24,6 +26,15 @@ public class FociBoundaryActivity extends BaseNavigationDrawerActivity {
     private static final String TAG = FociBoundaryActivity.class.getName();
 
     private KujakuMapView kujakuMapView;
+    private FeatureCollection boundaryFeatureCollection1;
+    private FeatureCollection boundaryFeatureCollection2;
+
+    private LatLng focusPoint1;
+    private LatLng focusPoint2;
+
+    private BoundaryLayer boundaryLayer;
+
+    private boolean showingBoundary1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,45 +44,63 @@ public class FociBoundaryActivity extends BaseNavigationDrawerActivity {
         kujakuMapView.onCreate(savedInstanceState);
 
         try {
-            FeatureCollection featureCollection = FeatureCollection.fromJson(
+            boundaryFeatureCollection1 = FeatureCollection.fromJson(
                     IOUtil.readInputStreamAsString(getAssets().open("coast-province-and-mti_18.geojson"))
             );
+            boundaryFeatureCollection2 = FeatureCollection.fromFeature(Feature.fromJson(
+                    IOUtil.readInputStreamAsString(getAssets().open("alternative_boundary.geojson"))
+            ));
+            focusPoint1 = new LatLng(-14.105596638939968, 32.60676728350983);
+            focusPoint2 = new LatLng(-14.157028852467521, 32.64508008956909);
 
-            BoundaryLayer.Builder builder = new BoundaryLayer.Builder(featureCollection)
+
+            BoundaryLayer.Builder builder = new BoundaryLayer.Builder(boundaryFeatureCollection1)
                     .setLabelProperty("name")
                     .setLabelTextSize(20f)
                     .setLabelColorInt(Color.RED)
                     .setBoundaryColor(Color.RED)
                     .setBoundaryWidth(6f);
 
-            BoundaryLayer boundaryLayer = builder.build();
+            boundaryLayer = builder.build();
             kujakuMapView.addLayer(boundaryLayer);
+            showingBoundary1 = true;
+
+            findViewById(R.id.btn_fociBoundary_changeBtn)
+                    .setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (showingBoundary1) {
+                                showingBoundary1 = false;
+                                boundaryLayer.updateFeatures(boundaryFeatureCollection2);
+                                changeFocus(focusPoint2);
+                            } else {
+                                showingBoundary1 = true;
+                                boundaryLayer.updateFeatures(boundaryFeatureCollection1);
+                                changeFocus(focusPoint1);
+                            }
+                        }
+                    });
 
             kujakuMapView.getMapAsync(new OnMapReadyCallback() {
                 @Override
                 public void onMapReady(MapboxMap mapboxMap) {
                     // Zoom to the position
                     mapboxMap.setStyle(Style.MAPBOX_STREETS);
-                    mapboxMap.easeCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-14.105596638939968, 32.60676728350983), 15d));
-
-                    new Thread(() -> {
-                        try {
-                            Thread.sleep(6000);
-
-                            String featureCollectionString = IOUtil.readInputStreamAsString(getAssets().open("alternative_boundary.geojson"));
-
-                            runOnUiThread(() -> {
-                                boundaryLayer.updateFeatures(FeatureCollection.fromFeature(Feature.fromJson(featureCollectionString)));
-                            });
-                        } catch (InterruptedException | IOException e) {
-                            Log.e(TAG, Log.getStackTraceString(e));
-                        }
-                    }).start();
+                    mapboxMap.easeCamera(CameraUpdateFactory.newLatLngZoom(focusPoint1, 15d));
                 }
             });
         } catch (IOException e) {
             Log.e(TAG, Log.getStackTraceString(e));
         }
+    }
+
+    private void changeFocus(@NonNull LatLng point) {
+        kujakuMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.easeCamera(CameraUpdateFactory.newLatLngZoom(point, 15d));
+            }
+        });
     }
 
     @Override
