2986540d4fbf2589c0baed7c8ccaed06610ac42a
Ephraim Kigamba <ekigamba@ona.io>
2019-05-22 15:22:40 +0300

    Revert to PopupMenu & fix checkable
    - Add the default base layer functionality

--- library/src/main/java/io/ona/kujaku/plugin/switcher/BaseLayerSwitcherPlugin.java
+++ library/src/main/java/io/ona/kujaku/plugin/switcher/BaseLayerSwitcherPlugin.java
@@ -1,22 +1,13 @@
 package io.ona.kujaku.plugin.switcher;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
+import android.support.v7.widget.PopupMenu;
 import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.PopupWindow;
-import android.widget.Toast;
 
 import com.mapbox.mapboxsdk.maps.Style;
 
@@ -31,18 +22,17 @@ import timber.log.Timber;
  * Created by Ephraim Kigamba - ekigamba@ona.io on 2019-05-16
  */
 
-public class BaseLayerSwitcherPlugin implements AdapterView.OnItemClickListener {
+public class BaseLayerSwitcherPlugin implements PopupMenu.OnMenuItemClickListener {
 
     private KujakuMapView kujakuMapView;
     private Style style;
-    private FloatingActionButton layerSwitcherFab;
+    private FloatingActionButton layerSwitcherFab;;
 
     @Nullable
     private BaseLayer currentBaseLayer;
 
     private ArrayList<BaseLayer> baseLayers = new ArrayList<>();
-    private PopupWindow listPopupWindow;
-    private ArrayAdapter<String> popupWindowAdapter;
+    private PopupMenu popupMenu;
 
     public BaseLayerSwitcherPlugin(@NonNull KujakuMapView kujakuMapView, @NonNull Style style) {
         this.kujakuMapView = kujakuMapView;
@@ -52,25 +42,23 @@ public class BaseLayerSwitcherPlugin implements AdapterView.OnItemClickListener 
     public boolean addBaseLayer(@NonNull BaseLayer baseLayer, boolean isDefault) {
         if (baseLayers.contains(baseLayer)) {
             // Todo: switch the currentBaseLayer to this one here
-            if (isDefault)
+            if (isDefault && currentBaseLayer != null && !currentBaseLayer.getId().equals(baseLayer.getId())) {
+                showBaseLayer(baseLayer);
+            }
+
             return false;
         }
 
         for (BaseLayer addedBaseLayer: baseLayers) {
             if (addedBaseLayer.getId().equals(baseLayer.getId())) {
-                // Todo: switch the currentBaseLayer to this one here
                 return false;
             }
         }
 
         this.baseLayers.add(baseLayer);
-        if (listPopupWindow != null) {
-            if (popupWindowAdapter != null) {
-                popupWindowAdapter.add(baseLayer.getDisplayName());
-            } else {
-                Timber.e("Cannot update popupWindowAdapter because it is null");
-            }
-
+        // Todo: switch the currentBaseLayer to this one here
+        if (isDefault) {
+            showBaseLayer(baseLayer);
         }
         return true;
     }
@@ -82,28 +70,11 @@ public class BaseLayerSwitcherPlugin implements AdapterView.OnItemClickListener 
 
     public boolean removeBaseLayer(@NonNull BaseLayer baseLayer) {
         if (baseLayers.remove(baseLayer)) {
-            if (listPopupWindow != null) {
-                if (popupWindowAdapter != null) {
-                    popupWindowAdapter.remove(baseLayer.getDisplayName());
-                } else {
-                    Timber.e("Cannot update popupWindowAdapter because it is null");
-                }
-
-            }
             return true;
         } else {
             for (BaseLayer addedBaseLayer : baseLayers) {
                 if (addedBaseLayer.getId().equals(baseLayer.getId())) {
                     baseLayers.remove(addedBaseLayer);
-
-                    if (listPopupWindow != null) {
-                        if (popupWindowAdapter != null) {
-                            popupWindowAdapter.remove(baseLayer.getDisplayName());
-                        } else {
-                            Timber.e("Cannot update popupWindowAdapter because it is null");
-                        }
-
-                    }
                     return true;
                 }
             }
@@ -128,23 +99,14 @@ public class BaseLayerSwitcherPlugin implements AdapterView.OnItemClickListener 
             }
 
             layerSwitcherFab.setVisibility(View.VISIBLE);
+
             layerSwitcherFab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (listPopupWindow == null) {
-
-                        if (baseLayers.size() > 0) {
-                            showPopup(layerSwitcherFab);
-                        } else {
-                            Toast.makeText(kujakuMapView.getContext(), R.string.no_base_layers_availabe, Toast.LENGTH_LONG)
-                                    .show();
-                        }
+                    if (popupMenu == null) {
+                        showPopup(layerSwitcherFab);
                     } else {
-                        if (listPopupWindow.isShowing()) {
-                            listPopupWindow.dismiss();
-                        } else {
-                            showPopup(layerSwitcherFab);
-                        }
+                        popupMenu.dismiss();
                     }
                 }
             });
@@ -152,69 +114,62 @@ public class BaseLayerSwitcherPlugin implements AdapterView.OnItemClickListener 
     }
 
     public void showPopup(@NonNull View anchorView) {
-        if (listPopupWindow == null) {
-            Context context = kujakuMapView.getContext();
-            listPopupWindow = new PopupWindow(context, null, 0, R.style.Widget_AppCompat_Light_ListPopupWindow);
-            listPopupWindow.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
-            listPopupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        popupMenu = new PopupMenu(kujakuMapView.getContext(), anchorView, Gravity.RIGHT, 0, R.style.PopupMenu);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.layer_switcher_popup_menu);
 
-            View view = LayoutInflater.from(context).inflate(R.layout.popupwindow_layer_switcher_selector, null);
-            listPopupWindow.setContentView(view);
+        // Add the other items
+        Menu menu = popupMenu.getMenu();
 
-            String[] layerNames = new String[getBaseLayers().size()];
+        int counter = 0;
+        for (BaseLayer baseLayer: getBaseLayers()) {
+            MenuItem menuItem = menu.add(R.id.checkableLayerGroup, baseLayer.hashCode(), counter, baseLayer.getDisplayName());
+            menuItem.setCheckable(true);
 
-            int counter = 0;
-            int selectedPosition = -1;
-            for (BaseLayer baseLayer : getBaseLayers()) {
-                layerNames[counter] = baseLayer.getDisplayName();
-
-                if (currentBaseLayer != null && (baseLayer == currentBaseLayer || baseLayer.getId().equals(currentBaseLayer.getId()))) {
-                    selectedPosition = counter;
-                }
-
-                counter++;
+            if (currentBaseLayer != null && (baseLayer == currentBaseLayer || baseLayer.getId().equals(currentBaseLayer.getId()))) {
+                menu.getItem(counter).setChecked(true);
             }
 
-            popupWindowAdapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, layerNames);
-            ListView listView = view.findViewById(R.id.lv_popupWindowLayerSwitcher_layerList);
-            listView.setAdapter(popupWindowAdapter);
+            counter++;
+        }
 
-            listView.setOnItemClickListener(this);
-            listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-
-            if (selectedPosition != -1) {
-                listView.setItemChecked(selectedPosition, true);
+        popupMenu.setOnDismissListener(new PopupMenu.OnDismissListener() {
+            @Override
+            public void onDismiss(PopupMenu menu) {
+                popupMenu = null;
             }
-        }
-
-        //listPopupWindow.showAsDropDown(anchorView);
-        Context context = anchorView.getContext();
-
-        if (context instanceof Activity) {
-            DisplayMetrics displayMetrics = new DisplayMetrics();
-            ((Activity) context).getWindowManager()
-                    .getDefaultDisplay()
-                    .getMetrics(displayMetrics);
-
-            int xOffset = displayMetrics.widthPixels - Float.valueOf(anchorView.getX()).intValue();
-
-            int[] screenPosition = new int[2];
-            kujakuMapView.getLocationOnScreen(screenPosition);
-            int yOffset = displayMetrics.heightPixels - (screenPosition[1] + kujakuMapView.getMeasuredHeight());
-
-            listPopupWindow.showAtLocation(anchorView, Gravity.BOTTOM | Gravity.RIGHT, xOffset, yOffset);
-        }
+        });
+        popupMenu.show();
     }
 
-    private int getPx(@NonNull Context context, float dpValue) {
-        Resources r = context.getResources();
-        float px = TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                dpValue,
-                r.getDisplayMetrics()
-        );
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if (currentBaseLayer != null && item.getItemId() == currentBaseLayer.hashCode()) {
+            return true;
+        }
 
-        return Float.valueOf(px).intValue();
+        for (BaseLayer baseLayer: baseLayers) {
+            if (item.getItemId() == baseLayer.hashCode()) {
+
+                // Disable the current checked item
+                if (popupMenu != null) {
+                    int size = popupMenu.getMenu().size();
+
+                    for (int i = 0; i < size; i++) {
+                        MenuItem menuItem = popupMenu.getMenu().getItem(i);
+                        if (menuItem.isChecked()) {
+                            menuItem.setChecked(false);
+                        }
+                    }
+                }
+
+                item.setChecked(true);
+                showBaseLayer(baseLayer);
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public void showBaseLayer(@NonNull BaseLayer baseLayer) {
@@ -237,23 +192,5 @@ public class BaseLayerSwitcherPlugin implements AdapterView.OnItemClickListener 
 
     private void removePreviousBaseLayer(@NonNull BaseLayer baseLayer) {
         kujakuMapView.removeLayer(baseLayer);
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        if (parent.getSelectedItemPosition() == position) {
-            return;
-        }
-
-        String displayName = (String) parent.getItemAtPosition(position);
-        for (BaseLayer baseLayer: baseLayers) {
-            if (baseLayer.getDisplayName().equals(displayName)) {
-
-                // Disable the current checked item
-                view.setSelected(true);
-                parent.setSelection(position);
-                showBaseLayer(baseLayer);
-            }
-        }
     }
 }
