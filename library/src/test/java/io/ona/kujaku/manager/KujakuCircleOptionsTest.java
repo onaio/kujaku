1d9e8797cbd852622090a885199e1d7fcc5a47d8
eotin <eo@novel-t.ch>
2019-06-26 14:23:07 +0200

    Improve Test coverage

--- library/src/test/java/io/ona/kujaku/manager/KujakuCircleOptionsTest.java
+++ library/src/test/java/io/ona/kujaku/manager/KujakuCircleOptionsTest.java
@@ -1,5 +1,9 @@
 package io.ona.kujaku.manager;
 
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.plugins.annotation.Circle;
+import com.mapbox.mapboxsdk.plugins.annotation.CircleOptions;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,5 +34,26 @@ public class KujakuCircleOptionsTest extends BaseTest {
         Assert.assertTrue(optionsMiddle.getMiddleCircle());
         Assert.assertFalse(options.getMiddleCircle());
         Assert.assertFalse(optionsDraggable.getMiddleCircle());
+    }
+
+    @Test
+    public void testCustomKujakuCircleOptions() {
+
+        KujakuCircleOptions kujakuOptions = new KujakuCircleOptions()
+                .withCircleBlur(10f)
+                .withCircleOpacity(5f)
+                .withCircleStrokeWidth(4f)
+                .withCircleColor("red")
+                .withCircleStrokeOpacity(2f)
+                .withGeometry(Point.fromLngLat(1,2));
+
+
+        CircleOptions option = (CircleOptions) kujakuOptions;
+
+        Assert.assertEquals(option.getCircleBlur(), kujakuOptions.getCircleBlur());
+        Assert.assertEquals(option.getCircleOpacity(), kujakuOptions.getCircleOpacity());
+        Assert.assertEquals(option.getCircleStrokeWidth(), kujakuOptions.getCircleStrokeWidth());
+        Assert.assertEquals(option.getCircleColor(), kujakuOptions.getCircleColor());
+        Assert.assertEquals(option.getCircleStrokeOpacity(), kujakuOptions.getCircleStrokeOpacity());
     }
 }
