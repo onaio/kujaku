ada2edf4eae1bf8fd0bceac98dc95b8407289cc9
eotin <eo@novel-t.ch>
2019-07-05 11:03:37 +0200

    Add OnSplittingLongClickListener

--- library/src/main/java/io/ona/kujaku/manager/options/SplittingManagerDefaultOptions.java
+++ library/src/main/java/io/ona/kujaku/manager/options/SplittingManagerDefaultOptions.java
@@ -1,5 +1,10 @@
 package io.ona.kujaku.manager.options;
 
+/**
+ * Default Splitting Manager Options class
+ *
+ * Created by Emmanuel Otin - eo@novel-t.ch on 05/07/2019
+ */
 public class SplittingManagerDefaultOptions extends SplittingManagerOptions {
 
     public SplittingManagerDefaultOptions() {
