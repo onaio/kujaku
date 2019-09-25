6ff04690de96189db08cb2e83fce1785307298e7
Samuel Githengi <samuel.githengi@gmail.com>
2019-09-25 12:57:56 +0300

    Change to android text utils

--- utils/src/main/java/io/ona/kujaku/utils/helpers/MapBoxStyleHelper.java
+++ utils/src/main/java/io/ona/kujaku/utils/helpers/MapBoxStyleHelper.java
@@ -2,8 +2,8 @@ package io.ona.kujaku.utils.helpers;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 
-import com.mapbox.core.utils.TextUtils;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 
 import org.json.JSONArray;
