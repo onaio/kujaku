6a112cb0aef90e960120cd6a4359d0a99a4fc275
Samuel Githengi <samuel.githengi@gmail.com>
2019-09-25 15:32:00 +0300

    Initialize Thread utils in debug mode

--- library/src/test/java/io/ona/kujaku/layers/BaseKujakuLayerTest.java
+++ library/src/test/java/io/ona/kujaku/layers/BaseKujakuLayerTest.java
@@ -1,18 +1,23 @@
 package io.ona.kujaku.layers;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.support.annotation.NonNull;
 
 import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.utils.ThreadUtils;
 
+import org.junit.Before;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import io.ona.kujaku.BaseTest;
+import io.ona.kujaku.test.shadows.ShadowCircleLayer;
 import io.ona.kujaku.test.shadows.ShadowFillLayer;
 import io.ona.kujaku.test.shadows.ShadowGeoJsonSource;
 import io.ona.kujaku.test.shadows.ShadowLayer;
 import io.ona.kujaku.test.shadows.ShadowLineLayer;
 import io.ona.kujaku.test.shadows.ShadowSymbolLayer;
-import io.ona.kujaku.test.shadows.ShadowCircleLayer;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,6 +31,13 @@ import static org.junit.Assert.assertEquals;
         ShadowLayer.class,
         ShadowCircleLayer.class})
 public abstract class BaseKujakuLayerTest extends BaseTest {
+
+    @Before
+    public void setUp() {
+        Context context = RuntimeEnvironment.application;
+        context.getApplicationInfo().flags = ApplicationInfo.FLAG_DEBUGGABLE;
+        ThreadUtils.init(RuntimeEnvironment.application);
+    }
 
     protected void assertPointEquals(@NonNull Point expected, @NonNull Point actual) {
         assertEquals(expected.latitude(), actual.latitude(), 0d);
