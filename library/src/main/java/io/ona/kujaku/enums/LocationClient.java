42c5060cc9dcad07e4e7c916a33f3df36fc9c62f
Ephraim Kigamba <ekigamba@ona.io>
2019-11-22 18:49:37 +0300

    Make KujakuMapView locationClient configurable through xml attributes

---
+++ library/src/main/java/io/ona/kujaku/enums/LocationClient.java
@@ -0,0 +1,10 @@
+package io.ona.kujaku.enums;
+
+/**
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 2019-11-22
+ */
+
+public enum LocationClient {
+    android_gps_client,
+    google_location_client
+}
